0b68e7e715308043150c2a8ddd01f4e35ba8fa0d
shivamanibrt <shivamanibrt03350@gmail.com>
2023-04-16 11:23:48 +1000

    fixed form

--- src/Components/ContactUsFile.js
+++ src/Components/ContactUsFile.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from "react";
 import emailjs from "emailjs-com";
-import { Row, Col, Toast } from "react-bootstrap";
+import { Row, Col, Toast, Container } from "react-bootstrap";
 
 const ContactUsFile = ({ isDarkMode }) => {
     const bgStyle = isDarkMode ? { backgroundColor: '#212529' } : { backgroundColor: 'rgb(244, 241, 241)' };
@@ -51,72 +51,75 @@ const ContactUsFile = ({ isDarkMode }) => {
 
     return (
         <>
-            <div className='p-4 ' style={bgStyle}>
-                <Row className=" text-light">
-                    <Col xs={12} md={8} className="mx-auto">
-                        <div className="email-form p-5 rounded">
-                            <h2 className="text-center mb-4" style={titleStyle}>Send Message</h2>
-                            <form ref={form} onSubmit={sendEmail} className="shadow-lg p-5 rounded-2">
-                                <div className="form-group mb-3">
-                                    <label htmlFor="user_name"><h4 style={titleStyle}>Full Name</h4></label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        name="user_name"
-                                        id="user_name"
-                                        value={formData.user_name}
-                                        onChange={handleInputChange}
-                                        placeholder="Full Name"
-                                        required
-                                    />
-                                </div>
-                                <div className="form-group mb-3">
-                                    <label htmlFor="user_email"><h4 style={titleStyle}>Email</h4></label>
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        name="user_email"
-                                        id="user_email"
-                                        value={formData.user_email}
-                                        onChange={handleInputChange}
-                                        placeholder="Email"
-                                        required
-                                    />
-                                </div>
-                                <div className="form-group mb-4">
-                                    <label htmlFor="message"><h4 style={titleStyle}>Message</h4></label>
-                                    <textarea
-                                        className="form-control"
-                                        name="message"
-                                        id="message"
-                                        rows="6"
-                                        style={{ resize: "none" }}
-                                        value={formData.message}
-                                        onChange={handleInputChange}
-                                    ></textarea>
-                                </div>
-                                <div className="text-center">
-                                    <button
-                                        type="submit"
-                                        className="btn text-light px-4 py-2 bg-danger"
-                                    >
-                                        Send Message
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </Col>
-                </Row>
-                <Toast
-                    show={showToast}
-                    onClose={() => setShowToast(false)}
-                    className="position-fixed top-0 end-0 m-3"
-                    delay={1000}
-                    autohide
-                >
-                    <Toast.Body>Message sent!</Toast.Body>
-                </Toast>
-            </div>
+            <Row>
+                <div className='p-4 ' style={bgStyle}>
+                    <Row className=" text-light">
+                        <Col xs={12} md={8} className="mx-auto">
+                            <div className="email-form p-5 rounded">
+                                <h2 className="text-center mb-4" style={titleStyle}>Send Message</h2>
+                                <form ref={form} onSubmit={sendEmail} className="shadow-lg p-5 rounded-2">
+                                    <div className="form-group mb-3">
+                                        <label htmlFor="user_name"><h4 style={titleStyle}>Full Name</h4></label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="user_name"
+                                            id="user_name"
+                                            value={formData.user_name}
+                                            onChange={handleInputChange}
+                                            placeholder="Full Name"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="form-group mb-3">
+                                        <label htmlFor="user_email"><h4 style={titleStyle}>Email</h4></label>
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            name="user_email"
+                                            id="user_email"
+                                            value={formData.user_email}
+                                            onChange={handleInputChange}
+                                            placeholder="Email"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="form-group mb-4">
+                                        <label htmlFor="message"><h4 style={titleStyle}>Message</h4></label>
+                                        <textarea
+                                            className="form-control"
+                                            name="message"
+                                            id="message"
+                                            rows="6"
+                                            style={{ resize: "none" }}
+                                            value={formData.message}
+                                            onChange={handleInputChange}
+                                        ></textarea>
+                                    </div>
+                                    <div className="text-center">
+                                        <button
+                                            type="submit"
+                                            className="btn text-light px-4 py-2 bg-danger"
+                                        >
+                                            Send Message
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Toast
+                        show={showToast}
+                        onClose={() => setShowToast(false)}
+                        className="position-fixed top-0 end-0 m-3"
+                        delay={1000}
+                        autohide
+                    >
+                        <Toast.Body>Message sent!</Toast.Body>
+                    </Toast>
+                </div>
+            </Row>
+
         </>
     );
 };
